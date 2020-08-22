7dad23fb185c4e974eb27724c8e4fcbc361a8981
ShirleyTZ <553728720@qq.com>
2020-08-22 18:10:31 +0800

    Updates

---
+++ precache-manifest.a90a196769838a6fbdee5c7b153849b6.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55ca0e845d69beb6f13bcf786593f044",
+    "url": "/my-web/index.html"
+  },
+  {
+    "revision": "5689ab71a0fee45966f0",
+    "url": "/my-web/static/css/2.ab8a0683.chunk.css"
+  },
+  {
+    "revision": "b19389e2feffe42fb9f7",
+    "url": "/my-web/static/css/main.dc943f70.chunk.css"
+  },
+  {
+    "revision": "5689ab71a0fee45966f0",
+    "url": "/my-web/static/js/2.b5f45046.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-web/static/js/2.b5f45046.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b19389e2feffe42fb9f7",
+    "url": "/my-web/static/js/main.71a51bee.chunk.js"
+  },
+  {
+    "revision": "426481e104698ad260b8",
+    "url": "/my-web/static/js/runtime-main.c9929950.js"
+  },
+  {
+    "revision": "9b63d334fc9fa4676c99f408d12519f3",
+    "url": "/my-web/static/media/Clothes.9b63d334.jpg"
+  },
+  {
+    "revision": "46b1abfb9eb39a82303e0025b7d87829",
+    "url": "/my-web/static/media/FrontImg.46b1abfb.jpg"
+  },
+  {
+    "revision": "a38864546d60ac9a3af766e8cd4615dd",
+    "url": "/my-web/static/media/Pic1.a3886454.jpg"
+  },
+  {
+    "revision": "5428abecb23c10699e21a562fabf1409",
+    "url": "/my-web/static/media/Pic2.5428abec.jpg"
+  },
+  {
+    "revision": "e8369759176966425f8740d5045f1baf",
+    "url": "/my-web/static/media/Pic3.e8369759.jpg"
+  },
+  {
+    "revision": "bc6596580df3d0f95ddaa59e2b15fb7e",
+    "url": "/my-web/static/media/Pic4.bc659658.jpg"
+  },
+  {
+    "revision": "09ca956eb9ffbd294ad9f1b35bdb3fca",
+    "url": "/my-web/static/media/Pic5.09ca956e.jpg"
+  },
+  {
+    "revision": "231ab257f467ac7a4d9b340dd04e2ea8",
+    "url": "/my-web/static/media/UserImg.231ab257.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-web/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "de0fc66c4ab0f2f6f778cfee8221d808",
+    "url": "/my-web/static/media/resumeImg.de0fc66c.jpg"
+  }
+]);
